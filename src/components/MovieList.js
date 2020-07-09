b4be87a39ff784a8c1a63012263babf84d944eaa
Pedro Júnior <pedro.delima@dataprev.gov.br>
2020-07-09 08:06:43 -0300

    Ordena as listas. Colei muito.

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const SORT_BY = {
+  NAME: 'name',
+  ACADEMY_AWARDS: 'academyAwardWins',
+};
+
+const getSortedMovies = (movies, sortBy) => {
+  if (sortBy === SORT_BY.ACADEMY_AWARDS) {
+    return movies
+      .slice()
+      .sort((a, b) => b[SORT_BY.ACADEMY_AWARDS] - a[SORT_BY.ACADEMY_AWARDS]);
+  } else {
+    return movies
+      .slice()
+      .sort((a, b) => a[SORT_BY.NAME].localeCompare(b[SORT_BY.NAME]));
+  }
+};
 
 const MovieList = ({ title, movies, onAddBookmark, onAddWatch, onRemove }) => {
+  const [sortBy, setSortBy] = useState(SORT_BY.ACADEMY_AWARDS);
+  const sortedMovies = getSortedMovies(movies, sortBy);
+
   return (
     <div className="movie-list">
       <h3>
         <span>{title}</span>
-        <button className="sort-button">
+        <button
+          className="sort-button"
+          onClick={() => setSortBy(SORT_BY.ACADEMY_AWARDS)}
+        >
           <i className="fa fa-sort-amount-desc"></i>
         </button>
-        <button className="sort-button">
+        <button className="sort-button" onClick={() => setSortBy(SORT_BY.NAME)}>
           <i className="fa fa-sort-alpha-asc"></i>
         </button>
       </h3>
       <ul>
-        {movies.map(movie => {
+        {sortedMovies.map(movie => {
           return (
             <li className="movie-list-item" key={movie._id}>
               <div>{movie.name}</div>
