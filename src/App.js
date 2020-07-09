f3c01aacfb56fb29e29679fa0b7cf89baeb06f90
Pedro Júnior <pedro.delima@dataprev.gov.br>
2020-07-09 09:05:40 -0300

    Adiciona react-router e retira DumbRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Menu from './components/Menu';
-import DumbRouter from './components/DumbRouter';
 
 const App = () => {
   return (
     <div className="app">
       <Menu />
-      <DumbRouter />
     </div>
   );
 };
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from '../pages/Home';
+import Movies from '../pages/Movies';
 
 const Menu = () => {
   return (
-    <div className="menu">
-      <h3>Menu</h3>
-      <ul className="menu-items">
-        <li className="menu-item">
-          <a href="/">Home</a>
-        </li>
-        <li className="menu-item">
-          <a href="/movies">Movies</a>
-        </li>
-      </ul>
-    </div>
+    <Router>
+      <div className="menu">
+        <h3>Menu</h3>
+        <ul className="menu-items">
+          <li className="menu-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="menu-item">
+            <Link to="/movies">Movies</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/movies">
+            <Movies />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
