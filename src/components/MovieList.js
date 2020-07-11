ccd01afb17e3def60c1d2f6ffaea17385ec5da20
Pedro Júnior <pedro.delima@dataprev.gov.br>
2020-07-11 17:58:57 -0300

    refatora sort de movies

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,77 +1,85 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
 const SORT_BY = {
-  NAME: 'name',
-  ACADEMY_AWARDS: 'academyAwardWins',
+  NAME: {
+    getSort: (a, b) => a.name.localeCompare(b.name),
+  },
+  ACADEMY_AWARDS: {
+    getSort: (a, b) => b.academyAwardWins - a.academyAwardWins,
+  },
 };
 
-const sortMovies = (movieA, movieB, sortBy) => {
-  if (sortBy === SORT_BY.ACADEMY_AWARDS) {
-    return movieB[SORT_BY.ACADEMY_AWARDS] - movieA[SORT_BY.ACADEMY_AWARDS];
-  } else {
-    return movieA[SORT_BY.NAME].localeCompare(movieB[SORT_BY.NAME]);
+class MovieList extends Component {
+  state = {
+    sortBy: SORT_BY.ACADEMY_AWARDS,
+  };
+
+  setSortBy = sortType => {
+    this.setState({ sortBy: sortType });
+  };
+
+  render() {
+    const { sortBy } = this.state;
+    const { title, movies, onUpdateMovie } = this.props;
+    const sortedMovies = movies.slice().sort(sortBy.getSort);
+
+    return (
+      <div className="movie-list">
+        <h3>
+          <span>{title}</span>
+          <button
+            className="sort-button"
+            onClick={() => this.setSortBy(SORT_BY.ACADEMY_AWARDS)}
+          >
+            <i className="fa fa-sort-amount-desc"></i>
+          </button>
+          <button
+            className="sort-button"
+            onClick={() => this.setSortBy(SORT_BY.NAME)}
+          >
+            <i className="fa fa-sort-alpha-asc"></i>
+          </button>
+        </h3>
+        <ul>
+          {sortedMovies.map(movie => {
+            return (
+              <li className="movie-list-item" key={movie._id}>
+                <div>{movie.name}</div>
+                <div>Academy Awards: {movie.academyAwardWins}</div>
+                <span>
+                  <button
+                    onClick={() => onUpdateMovie(movie._id, 'bookmarked', true)}
+                  >
+                    <i className="fa fa-star"></i>
+                  </button>
+                </span>
+                <span>
+                  <button
+                    onClick={() => onUpdateMovie(movie._id, 'watched', true)}
+                  >
+                    <i className="fa fa-check"></i>
+                  </button>
+                </span>
+                <span>
+                  <button
+                    onClick={() =>
+                      onUpdateMovie(
+                        movie._id,
+                        title === 'Bookmarked' ? 'bookmarked' : 'watched',
+                        false
+                      )
+                    }
+                  >
+                    <i className="fa fa-times"></i>
+                  </button>
+                </span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
   }
-};
-
-const MovieList = ({ title, movies, onUpdateMovie }) => {
-  const [sortBy, setSortBy] = useState(SORT_BY.ACADEMY_AWARDS);
-  const sortedMovies = movies
-    .slice()
-    .sort((movieA, movieB) => sortMovies(movieA, movieB, sortBy));
-
-  return (
-    <div className="movie-list">
-      <h3>
-        <span>{title}</span>
-        <button
-          className="sort-button"
-          onClick={() => setSortBy(SORT_BY.ACADEMY_AWARDS)}
-        >
-          <i className="fa fa-sort-amount-desc"></i>
-        </button>
-        <button className="sort-button" onClick={() => setSortBy(SORT_BY.NAME)}>
-          <i className="fa fa-sort-alpha-asc"></i>
-        </button>
-      </h3>
-      <ul>
-        {sortedMovies.map(movie => {
-          return (
-            <li className="movie-list-item" key={movie._id}>
-              <div>{movie.name}</div>
-              <div>Academy Awards: {movie.academyAwardWins}</div>
-              <span>
-                <button
-                  onClick={() => onUpdateMovie(movie._id, 'bookmarked', true)}
-                >
-                  <i className="fa fa-star"></i>
-                </button>
-              </span>
-              <span>
-                <button
-                  onClick={() => onUpdateMovie(movie._id, 'watched', true)}
-                >
-                  <i className="fa fa-check"></i>
-                </button>
-              </span>
-              <span>
-                <button
-                  onClick={() =>
-                    onUpdateMovie(
-                      movie._id,
-                      title === 'Bookmarked' ? 'bookmarked' : 'watched',
-                      false
-                    )
-                  }
-                >
-                  <i className="fa fa-times"></i>
-                </button>
-              </span>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
-};
+}
 
 export default MovieList;
